0270cbdcad37baefe492ee5ca4c0cb49279d4e83
Vasanth Reddy Baddam <30804186+vbaddam@users.noreply.github.com>
2020-07-09 09:04:53 -0400

    Update 581_Shortest _Unsorted_Continuous_Subarray.py

--- Arrays/581_Shortest _Unsorted_Continuous_Subarray.py
+++ Arrays/581_Shortest _Unsorted_Continuous_Subarray.py
@@ -5,6 +5,7 @@ class Solution:
         if nums != snum:
             for i in range(len(nums)):
                 if snum[i] != nums[i]:
+                    
                     init.append(i)
             return(init[-1] - init[0] + 1)
         else:
